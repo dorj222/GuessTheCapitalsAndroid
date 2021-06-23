63731988ce778f456581e78e9264fdbd80ded014
dorj222 <dorj222@gmail.com>
2021-06-23 18:54:47 +0800

    added "Start" button

---
+++ app/src/main/java/us/wabash/guessthecapitals/GameMechanics.kt
@@ -0,0 +1,11 @@
+package us.wabash.guessthecapitals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameMechanics : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_mechanics)
+    }
+}
